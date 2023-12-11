6d9ed581f883fcfced3d241f512a83edfae55bcc
developervicky <developer.vicky6@gmail.com>
2023-12-11 15:50:20 +0530

    Environment Setup and Topbar UI

---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import HomeComponent from "../components/HomeComponent";
+function Homepage() {
+  return (
+    <div>
+      <HomeComponent />
+    </div>
+  );
+}
+
+export default Homepage;
