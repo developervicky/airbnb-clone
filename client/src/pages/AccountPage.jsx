8db7210f55416bd9d90d298ad3fb513195c04507
developervicky <developer.vicky6@gmail.com>
2023-12-19 16:25:18 +0530

    refactor: removed unnecessary imports and dependencies

--- client/src/pages/AccountPage.jsx
+++ client/src/pages/AccountPage.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import { useContext } from "react";
 import { UserContext } from "../components/UserContext";
 import { Navigate, useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
