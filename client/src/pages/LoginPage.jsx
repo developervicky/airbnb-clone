d7cd908cb2a39e6d47274b204dfcc85f31dbbd7a
developervicky <developer.vicky6@gmail.com>
2023-12-20 19:06:24 +0530

    refactor: removed unnecessary dependencies from login page

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,5 +1,5 @@
-import React, { useContext, useState } from "react";
-import { Link, Navigate, resolvePath } from "react-router-dom";
+import { useContext, useState } from "react";
+import { Link, Navigate } from "react-router-dom";
 import axios from "axios";
 import { Toastify } from "../common/toastify/Toastify.jsx";
 import { UserContext } from "../components/UserContext.jsx";
@@ -16,7 +16,7 @@ function LoginPage() {
   const signinUser = async (e) => {
     e.preventDefault();
     try {
-      const data = await axios
+      await axios
         .post("/signin", {
           email,
           password,
@@ -84,19 +84,19 @@ function LoginPage() {
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
-                stroke-width="1.5"
+                strokeWidth="1.5"
                 stroke="currentColor"
-                class="w-6 h-6"
+                className="w-6 h-6"
                 style={{ color: !hidePassword ? "#17A191" : "#c3c3c3" }}
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                   d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
                 />
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                   d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
                 />
               </svg>
