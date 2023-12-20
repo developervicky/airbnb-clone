ea07a6892c4b8654631a353ceb6e698f283cfc5f
developervicky <developer.vicky6@gmail.com>
2023-12-20 09:33:45 +0530

    refactor: directories updated

--- client/index.jsx
+++ client/index.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import router from "./router/router.jsx";
+import router from "./src/router/router.jsx";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { UserContextProvider } from "./components/UserContext.jsx";
+import { UserContextProvider } from "./src/components/UserContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
