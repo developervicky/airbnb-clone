8708830d1632235979aca387b16a4961d3dc3115
developervicky <developer.vicky6@gmail.com>
2023-12-23 15:45:29 +0530

    feat: Created Home page Accommodation modal

--- client/src/components/AddPlace.jsx
+++ client/src/components/AddPlace.jsx
@@ -14,34 +14,35 @@ export default function AddPlace() {
 
   return (
     <div className="flex flex-col grow">
-      <div className="pb-6">
-        <div
-          key={placeFind._id}
-          className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-10"
-        >
-          {placeFind.length > 0 &&
-            placeFind.map((acc) => (
-              <Link
-                key={acc.id}
-                to={`/account/accommodations/user/${acc._id}`}
-                className="flex flex-col items-center gap-3 cursor-pointer font-semibold "
-              >
-                <img
-                  src={"http://localhost:5000/uploads/" + acc.photos[0]}
-                  className=" rounded-xl w-full h-full"
-                ></img>
-                <div className="w-full px-2 flex flex-row gap-1 justify-between items-center">
-                  <div className="flex flex-col">
-                    <h2 className="tracking-wider ">{acc.title}</h2>
-                    <h2 className="font-medium text-gray-600 tracking-wider">
-                      {acc.city}, {acc.country}
-                    </h2>
+      <div
+        key={placeFind._id}
+        className="grid gap-12 grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
+      >
+        {placeFind.length > 0 &&
+          placeFind.map((acc) => (
+            <>
+              {acc.photos[0] && (
+                <Link className="flex flex-col gap-3">
+                  <img
+                    src={"http://localhost:5000/uploads/" + acc.photos[0]}
+                    alt="plcImg"
+                    className="aspect-square object-cover rounded-xl"
+                  />
+                  <div className="flex flex-row gap-10 justify-between items-center">
+                    <div className="flex flex-col truncate">
+                      <h2 className="text-lg tracking-wide font-bold truncate ">
+                        {acc.title}
+                      </h2>
+                      <h2 className="font-medium tracking-wider text-gray-400">
+                        {acc.city}, {acc.country}
+                      </h2>
+                    </div>
+                    <h2>{acc.price}</h2>
                   </div>
-                  <h2 className="font-semibold">{acc.price}</h2>
-                </div>
-              </Link>
-            ))}
-        </div>
+                </Link>
+              )}
+            </>
+          ))}
       </div>
       <div className="flex  flex-col grow items-end justify-end sticky bottom-0 pb-8">
         <Link
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,7 +1,54 @@
-import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { GoPlus } from "react-icons/go";
 function Homepage() {
+  const [accData, setAccData] = useState([]);
+  useEffect(() => {
+    axios.get("/home-place").then((res) => {
+      setAccData(res.data);
+    });
+  }, []);
   return (
-    <div>
+    <div className="w-3/4 mx-auto flex flex-col justify-between grow  ">
+      <div className="grid gap-12 grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        {accData.length > 0 &&
+          accData.map((acc) => (
+            <>
+              {acc.photos[0] && (
+                <Link className="flex flex-col gap-3">
+                  <img
+                    src={"http://localhost:5000/uploads/" + acc.photos[0]}
+                    alt="plcImg"
+                    className="aspect-square object-cover rounded-xl"
+                  />
+                  <div className="flex flex-row gap-10 justify-between items-center">
+                    <div className="flex flex-col truncate">
+                      <h2 className="text-lg tracking-wide font-bold truncate ">
+                        {acc.title}
+                      </h2>
+                      <h2 className="font-medium tracking-wider text-gray-400">
+                        {acc.city}, {acc.country}
+                      </h2>
+                    </div>
+                    <h2>{acc.price}</h2>
+                  </div>
+                </Link>
+              )}
+            </>
+          ))}
+      </div>
+      <div className="flex  flex-col grow items-end justify-end sticky bottom-0 pb-8">
+        <Link
+          to={"/account/accommodations/new"}
+          className="flex flex-row-reverse flex-row bg-white w-fit items-center justify-center px-4 gap-3 border-2 p-2 rounded-full cursor-pointer font-semibold  hover:border-primary hover:bg-primary hover:text-white"
+        >
+          <GoPlus className="text-3xl" />
+          <button className="tracking-wide font-semibold ">
+            Add New Place
+          </button>
+        </Link>
+      </div>
     </div>
   );
 }
