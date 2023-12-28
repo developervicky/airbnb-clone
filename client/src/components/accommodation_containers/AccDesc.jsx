d4739e3ac489c143ee4f97326958b15c5ae91d14
developervicky <developer.vicky6@gmail.com>
2023-12-28 11:08:46 +0530

    feat: read more and less component

--- client/src/components/accommodation_containers/AccDesc.jsx
+++ client/src/components/accommodation_containers/AccDesc.jsx
@@ -1,4 +1,11 @@
+import { useState } from "react";
+import { FaAngleDown, FaAngleUp } from "react-icons/fa";
+
 export default function AccDesc({ place }) {
+  const [readMore, setReadMore] = useState(false);
+
+  const description = place?.description;
+
   return (
     <>
       <div className="flex flex-col gap-3 py-7 pt-1 border-b-2 ">
@@ -6,7 +13,27 @@ export default function AccDesc({ place }) {
           Explore in {place?.city}, {place?.country}
         </h1>
         <p className=" tracking-wide leading-8 text-gray-600 md:text-lg xl:text-xl ">
-          {place?.description}
+          {readMore ? (
+            <p className="flex flex-row items-end">
+              {description}
+              <button
+                onClick={() => setReadMore(false)}
+                className=" text-sm sm:text-base text-white border-2 border-primary rounded-full bg-primary p-1 hover:bg-primaryl hover:border-primaryl"
+              >
+                <FaAngleUp />
+              </button>
+            </p>
+          ) : (
+            <p className="flex flex-row items-end">
+              {description.slice(0, 200)}....{" "}
+              <button
+                onClick={() => setReadMore(true)}
+                className=" text-sm sm:text-base text-white border-2 border-primary rounded-full bg-primary p-1 hover:bg-primaryl hover:border-primaryl"
+              >
+                <FaAngleDown />
+              </button>
+            </p>
+          )}
         </p>
       </div>
     </>
--- client/src/components/accommodation_containers/AccExtraInfo.jsx
+++ client/src/components/accommodation_containers/AccExtraInfo.jsx
@@ -1,4 +1,8 @@
+import { useState } from "react";
+import { FaAngleDown, FaAngleUp } from "react-icons/fa";
+
 export default function AccExtraInfo({ place }) {
+  const [readMore, setReadMore] = useState(false);
   return (
     <>
       <div className="flex w-fit flex-col gap-3 py-7 border-b-2">
@@ -6,7 +10,27 @@ export default function AccExtraInfo({ place }) {
           Other stuffs to note
         </h1>
         <p className="tracking-wide leading-8 text-gray-600 md:text-lg xl:text-xl">
-          {place?.extraInfo}
+          {readMore ? (
+            <p className="flex flex-row items-end">
+              {place.extraInfo}
+              <button
+                onClick={() => setReadMore(false)}
+                className=" text-sm sm:text-base text-white border-2 border-primary rounded-full bg-primary p-1 hover:bg-primaryl hover:border-primaryl"
+              >
+                <FaAngleUp />
+              </button>
+            </p>
+          ) : (
+            <p className="flex flex-row items-end">
+              {place.extraInfo.slice(0, 100)}....
+              <button
+                onClick={() => setReadMore(true)}
+                className=" text-sm sm:text-base text-white border-2 border-primary rounded-full bg-primary p-1 hover:bg-primaryl hover:border-primaryl"
+              >
+                <FaAngleDown />
+              </button>
+            </p>
+          )}
         </p>
       </div>
     </>
